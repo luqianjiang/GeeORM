e232c50cc773503fa7c38d2ad1c983d9c2d2ff2d
qianjianglu <qianjianglu@trip.com>
2023-08-24 16:26:42 +0800

    day4：init

---
+++ day4-chain-operation/dialect/dialect.go
@@ -0,0 +1,25 @@
+package dialect
+
+import "reflect"
+
+var dialectsMap = map[string]Dialect{}
+
+type Dialect interface {
+
+	// DataTypeOf 用于将 Go 语言的类型转换为该数据库的数据类型
+	DataTypeOf(typ reflect.Value) string
+
+	// TableExistSQL 返回某个表是否存在的 SQL 语句，参数是表名(table)
+	TableExistSQL(tableName string) (string, []interface{})
+}
+
+// RegisterDialect 注册 dialect（支持不同数据库）
+func RegisterDialect(name string, dialect Dialect) {
+	dialectsMap[name] = dialect
+}
+
+// GetDialect 获取 dialect
+func GetDialect(name string) (dialect Dialect, ok bool) {
+	dialect, ok = dialectsMap[name]
+	return
+}
---
+++ day4-chain-operation/session/record.go
@@ -0,0 +1,54 @@
+package session
+
+import (
+	"day4-chain-operation/clause"
+	"reflect"
+)
+
+// Insert 插入 入参数 ：参入对象
+func (s *Session) Insert(values ...interface{}) (int64, error) {
+	// recordValues 保存每个对象的值
+	recordValues := make([]interface{}, 0)
+	for _, value := range values {
+		table := s.Model(value).RefTable()
+		s.clause.Set(clause.INSERT, table.Name, table.FieldNames)
+		recordValues = append(recordValues, table.RecordValues(value))
+	}
+
+	s.clause.Set(clause.VALUES, recordValues...)
+	sql, vars := s.clause.Build(clause.INSERT, clause.VALUES)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+// reflect.New(destType) 将type类型转化为value类型
+func (s *Session) Find(values interface{}) error {
+	destSlice := reflect.Indirect(reflect.ValueOf(values))
+	destType := destSlice.Type().Elem()
+	table := s.Model(reflect.New(destType).Elem().Interface()).RefTable()
+
+	s.clause.Set(clause.SELECT, table.Name, table.FieldNames)
+	sql, vars := s.clause.Build(clause.SELECT, clause.WHERE, clause.ORDERBY, clause.LIMIT)
+	rows, err := s.Raw(sql, vars...).QueryRows()
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		dest := reflect.New(destType).Elem()
+		var values []interface{}
+		for _, name := range table.FieldNames {
+			// dest.FieldByName(name).Addr() 返回字段的地址，然后通过rows.Scan(values...)赋值给values，同时也修改了dest字段地址的值
+			values = append(values, dest.FieldByName(name).Addr().Interface())
+		}
+		if err := rows.Scan(values...); err != nil {
+			return err
+		}
+		// destSlice 间接指向入参values，因此修改destSlice也会修改入参values，
+		destSlice.Set(reflect.Append(destSlice, dest))
+	}
+	return rows.Close()
+}
