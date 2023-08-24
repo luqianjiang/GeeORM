9bdbc156dbb432de91c63e13f0853b186ae2f6dd
qianjianglu <qianjianglu@trip.com>
2023-08-24 15:09:19 +0800

    day3：记录新增和查询

---
+++ day3-save-query/session/raw.go
@@ -0,0 +1,76 @@
+package session
+
+import (
+	"database/sql"
+	"day3-save-query/clause"
+	"day3-save-query/dialect"
+	"day3-save-query/log"
+	"day3-save-query/schema"
+	"strings"
+)
+
+type Session struct {
+	db      *sql.DB
+	sql     strings.Builder
+	sqlVars []interface{}
+
+	dialect dialect.Dialect
+
+	// 表结构信息
+	refTable *schema.Schema
+
+	// 操作语句与操作值
+	clause clause.Clause
+}
+
+func New(db *sql.DB, dialect dialect.Dialect) *Session {
+	return &Session{
+		db:      db,
+		dialect: dialect,
+	}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+	s.clause = clause.Clause{}
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+// Raw appends sql and sqlVars
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
+
+// Exec raw sql with sqlVars
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear() // 清理之前的sql
+	log.Info(s.sql.String(), s.sqlVars)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+// QueryRow gets a record from db
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
+
+// QueryRows gets a list of records from db
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
---
+++ day3-save-query/session/table.go
@@ -0,0 +1,51 @@
+package session
+
+import (
+	"day3-save-query/log"
+	"day3-save-query/schema"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// Model 入参：对象
+func (s *Session) Model(value interface{}) *Session {
+
+	// nil or different model, update refTable
+	if s.refTable == nil || reflect.TypeOf(value) != reflect.TypeOf(s.refTable.Model) {
+		s.refTable = schema.Parse(value, s.dialect) // 解析表结构
+	}
+	return s
+}
+
+func (s *Session) RefTable() *schema.Schema {
+	if s.refTable == nil {
+		log.Error("Model is not set")
+	}
+	return s.refTable
+}
+
+func (s *Session) CreateTable() error {
+	table := s.RefTable()
+	var columns []string
+	for _, field := range table.Fields {
+		columns = append(columns, fmt.Sprintf("%s %s %s", field.Name, field.Type, field.Tag))
+	}
+	// 使用 ， 拼接起来
+	desc := strings.Join(columns, ",")
+	_, err := s.Raw(fmt.Sprintf("CREATE TABLE %s (%s);", table.Name, desc)).Exec()
+	return err
+}
+
+func (s *Session) DropTable() error {
+	_, err := s.Raw(fmt.Sprintf("DROP TABLE IF EXISTS %s", s.RefTable().Name)).Exec()
+	return err
+}
+
+func (s *Session) HasTable() bool {
+	sql, values := s.dialect.TableExistSQL(s.RefTable().Name)
+	row := s.Raw(sql, values...).QueryRow()
+	var tmp string
+	_ = row.Scan(&tmp)
+	return tmp == s.RefTable().Name
+}
